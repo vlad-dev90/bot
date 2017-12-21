fa9f32a700cda803c3c9417eaacf4acad8e8ddb5
vlad <vladst90@gmail.com>
2017-12-21 19:40:21 +0300

    First commit

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,21 @@
+class QuestionController < ApplicationController
+  def index
+    binding.pry
+  end
+  def quiz
+    # @respond = params.permit(:question, :id, :level)
+    head :ok
+    uri = URI("http://0.0.0.0:3000")
+    parameters = {
+      answer: 'ваш ответ',
+      token: 'API_KEY (см. процесс регистрации)',
+      task_id:  'id задачи (передано ранее)'
+    }
+    Net::HTTP.post_form(uri, parameters)
+
+  end
+
+  def test
+    binding.pry
+  end
+end
---
+++ test.rb
@@ -0,0 +1,12 @@
+require 'json'
+require 'pry'
+
+poems = JSON.parse(File.open('./db/poems.json').read)
+level1_poems = {}
+poems.each do |poem|
+  poem[1].each do |string|
+    level1_poems[string.strip.gsub(/[[:punct:]]\z/, '')] = poem[0]
+  end
+end
+binding.pry
+p 's'
